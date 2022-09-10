44bd1e3dec9d835d1af245c07b0759a7de429104
aptemkov <artkov37@gmail.com>
2022-09-10 19:14:43 +0300

    notifyDataSetChanged() replaced on DiffUtil

--- app/src/main/java/com/example/cleanarchitectureapp/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/cleanarchitectureapp/presentation/ShopListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cleanarchitectureapp.R
 import com.example.cleanarchitectureapp.domain.ShopItem
@@ -12,8 +13,12 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
 
     var shopList = listOf<ShopItem>()
     set(value) {
+        val callback = ShopListDiffCallback(shopList, value)
+        val diffResult = DiffUtil.calculateDiff(callback)
+        diffResult.dispatchUpdatesTo(this)
         field = value
-        notifyDataSetChanged()
+        // notifyDataSetChanged() вместо этого реализация выше. Лучше не использовать,
+        // так как перерисовывается весь список
     }
 
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
